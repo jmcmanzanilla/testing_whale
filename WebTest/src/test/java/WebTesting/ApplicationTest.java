8b8d877d4c56755e1233766bb3796a27cb8813d7
Joan Manzanilla <joan.m.c.manzanilla@accenture.com>
2016-09-15 17:35:29 +0800

    initial commit

---
+++ WebTest/src/test/java/WebTesting/ApplicationTest.java
@@ -0,0 +1,55 @@
+package WebTesting;
+
+//import java.util.regex.Pattern;
+//import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.support.ui.Select;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class ApplicationTest {
+	WebDriver driver; 
+	  String baseUrl, nodeUrl;
+	  private StringBuffer verificationErrors = new StringBuffer();
+	  
+	  @BeforeTest 
+		public void setUp() throws MalformedURLException {
+			nodeUrl = "http://selenium-hub:4444/wd/hub";
+			baseUrl = "http://52.53.188.36/SampleWebApp/";
+			DesiredCapabilities capability = DesiredCapabilities.firefox();
+			capability.setBrowserName("firefox");
+			driver = new RemoteWebDriver(new URL(nodeUrl), capability);	
+		}	
+	  
+	  @Test
+	  public void testVerifyTitle() throws Exception {
+	    driver.get(baseUrl);
+		assertEquals(driver.getTitle(), "Global Clock");
+	  }
+	  
+	  @Test
+	  public void testVerifyName() throws Exception {
+	    driver.get(baseUrl);
+	    driver.findElement(By.id("switzerland")).click();
+	    assertEquals(driver.findElement(By.id("country")).getText(), "SWITZERLAND");
+	  }
+	  
+	  @Test
+	  public void testVerifyTimeDiff() throws Exception {
+	    driver.get(baseUrl);
+	    driver.findElement(By.id("switzerland")).click();
+	    driver.findElement(By.cssSelector("button.buttonTime")).click();
+	    assertEquals(driver.findElement(By.id("answer1")).getText(), "The time difference between Switzerland and Philippines is 6 hours");
+	  }
+  
+  @AfterTest 
+	public void afterTest() { 
+	driver.quit(); 
+	} 
+}
